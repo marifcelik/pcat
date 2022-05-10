ba573f3baa904facbf7fdf50d9941ed0ac060aa2
arif <arif-celik56@hotmail.com>
2022-05-11 02:24:50 +0300

    sayfalar arası geçiş sağlandı

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
 const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 const path = require('path');
-const ejs = require('ejs');
+// const ejs = require('ejs');
 const app = express();
 const port = 3000;
 
-const logger = (res,req,next) => {
+const logger = (res, req, next) => {
     console.log('log deneme');
     next();
 }
 
-app.set('view engine', ejs);
-app.use(express.static('public'));
-app.use(logger);
+let sayfalar = [
+    { adres: '', content: 'index' },
+    { adres: 'index', content: 'index' },
+    { adres: 'about', content: 'about' },
+    { adres: 'contact', content: 'contact' }
+]
 
-app.get('/', (req,res) =>  {
-    res.sendFile(path.resolve(__dirname, './temp/index.html'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+// app.use(logger);
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+    console.log(req.url);
+    console.log(req.params);
+})
+
+app.get('/:adres', (req, res) => {
+    const sendFile = sayfalar.find(sayfa => sayfa.adres == req.params.adres)
+    console.log(req.params);
+    console.log(sendFile);
+    res.render(sendFile.content);
     console.log(req.ip);
 })
 
